fc01667d298136d6522e59e33bd0158737e1cae6
Honfi Botond <hobot2002@gmail.com>
2024-10-04 19:55:03 +0200

    String kijavítása

--- hon_bk2_temperature/alert_node.py
+++ hon_bk2_temperature/alert_node.py
@@ -1,26 +1,28 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Float32
+from std_msgs.msg import Float32, String
 
-class Alertnode(Node):
+class AlertNode(Node):
+
     def __init__(self):
         super().__init__('alert_node')
         self.subscription = self.create_subscription(Float32, 'temperature', self.check_temperature, 10)
-        self.publisher = self.create_publisher(str,'temperature_alert', 10)
+        self.publisher_ = self.create_publisher(String, 'temperature_alert', 10)
+        self.get_logger().info(f'Élek virulok')
 
-    def check_temperature(self,msg):
-        if (msg.data > 25.0):
-            alert_msg = str()
+    def check_temperature(self, msg):
+        if msg.data > 25.0:
+            alert_msg = String()
             alert_msg.data = "Hőmérséklet túl magas!"
-            self.publisher.publish(alert_msg)
-            self.get_logger().info('Alert: A hőmérséklet túl magas!!')
+            self.publisher_.publish(alert_msg)
+            self.get_logger().info('Alert: Hőmérséklet túl magas!')
 
 def main(args=None):
     rclpy.init(args=args)
-    node = Alertnode()
+    node = AlertNode()
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
 
 if __name__ == '__main__':
-    main()  
+    main()
